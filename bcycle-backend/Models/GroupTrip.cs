5d47a2b82f4f077585dd37a1755cdb44f914fc99
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2020-03-18 00:14:20 +0100

    Remove StartPoint/GetPoint properties due to conflicts and consistency issues

--- bcycle-backend/Models/GroupTrip.cs
+++ bcycle-backend/Models/GroupTrip.cs
@@ -15,10 +15,12 @@ namespace bcycle_backend.Models
         public string TripCode { get; set; }
         public string MapImageUrl { get; set; }
         public int HostID { get; set; }
+        //public int StartPointID {get; set; }
+        //public int EndPointID { get; set; }
 
         public User Host { get; set; }
-        public GroupTripPoint StartPoint{ get; set; }
-        public GroupTripPoint EndPoint{ get; set; }
+        //public GroupTripPoint StartPoint { get; set; }
+        //public GroupTripPoint EndPoint { get; set; }
         public List<GroupTripPoint> GroupTripPoints { get; set; }
         public List<GroupTripParticipant> Participants { get; set; }
     }
--- bcycle-backend/Models/Trip.cs
+++ bcycle-backend/Models/Trip.cs
@@ -14,10 +14,13 @@ namespace bcycle_backend.Models
         public string MapImageUrl { get; set; }
         public int HostUserID { get; set; }
         public int GroupTripID { get; set; }
+        //public int StartPointID { get; set; }
+        //public int EndPointID { get; set; }
+
         public User Host { get; set; }
         public GroupTrip GroupTrip { get; set; }
-        public TripPoint StartPoint { get; set; }
-        public TripPoint EndPoint { get; set; }
+        //public TripPoint StartPoint { get; set; }
+        //public TripPoint EndPoint { get; set; }
         public List<TripPoint> TripPoints { get; set; }
     }
 }
