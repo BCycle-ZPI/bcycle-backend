85c9e87b708f68727d8e17a2fcc2fce623e1752b
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2020-05-10 18:01:20 +0200

    Add database migration for SharingGuid

--- bcycle-backend/Migrations/20200510160048_AddSharingGuid.Designer.cs
+++ bcycle-backend/Migrations/20200510160048_AddSharingGuid.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using bcycle_backend.Data;
 
 namespace bcycle_backend.Migrations
 {
     [DbContext(typeof(BCycleContext))]
-    partial class BCycleContextModelSnapshot : ModelSnapshot
+    [Migration("20200510160048_AddSharingGuid")]
+    partial class AddSharingGuid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ bcycle-backend/Migrations/20200510160048_AddSharingGuid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace bcycle_backend.Migrations
+{
+    public partial class AddSharingGuid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SharingGuid",
+                table: "Trips",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SharingGuid",
+                table: "GroupTrips",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SharingGuid",
+                table: "Trips");
+
+            migrationBuilder.DropColumn(
+                name: "SharingGuid",
+                table: "GroupTrips");
+        }
+    }
+}
