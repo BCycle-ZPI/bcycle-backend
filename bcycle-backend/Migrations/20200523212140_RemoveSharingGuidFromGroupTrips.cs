e5c2d0f8bd7694672ff17ef786d9b26afb891c25
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2020-05-23 23:22:19 +0200

    Remove SharingGuid from GroupTrips

---
+++ bcycle-backend/Migrations/20200523212140_RemoveSharingGuidFromGroupTrips.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace bcycle_backend.Migrations
+{
+    public partial class RemoveSharingGuidFromGroupTrips : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SharingGuid",
+                table: "GroupTrips");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SharingGuid",
+                table: "GroupTrips",
+                nullable: true);
+        }
+    }
+}
